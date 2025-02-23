a7f95403627a438da1b0ea4b9a4078087f4d001c
Piksi-eng <bogosavljevicivan72@gmail.com>
2025-02-23 02:15:24 +0100

    added slider

--- ShushBot.py
+++ ShushBot.py
@@ -2,6 +2,7 @@ import numpy as np
 import sounddevice as sd
 from plyer import notification
 import tkinter as tk
+from tkinter import ttk
 import time
 from playsound import playsound
 import threading
@@ -56,22 +57,15 @@ def create_ring():
 def play_sound(file_path):
     playsound(file_path)
 
-
-
-# Example usage
-if __name__ == "__main__":
+def listen():
     ring = None
-    ring_position = (0, 0)
-    sound_file = 'D:\ShushBot\shushhh.mp3'
-    
     while True:
         max_loudness = get_max_loudness()
-        if(max_loudness > -10):
+        if(max_loudness > scale_float.get()):
             if ring is None:  # Create the ring if it doesn't exist
                 sound_thread = threading.Thread(target=play_sound, args=(sound_file,))
                 ring = create_ring()               
-                sound_thread.start()
-                #playsound("D:\ShushBot\shushhh.mp3")                
+                sound_thread.start()            
                 print(f"Maximum Loudness: {max_loudness:.2f} dB")
                 print("You are too loud!!")
             ring.deiconify()  # Show the ring
@@ -80,5 +74,23 @@ if __name__ == "__main__":
                 ring.withdraw()
                 ring = None
         time.sleep(0.1)
+
+# Example usage
+if __name__ == "__main__":
+    listen_thread = threading.Thread(target=listen)
+    listen_thread.start()
+    sound_file = 'shushhh.mp3'
+    threshold = -10
+    window = tk.Tk()
+    window.title("ShushBot")
+    scale_float = tk.DoubleVar(value = -10)
+    scale = ttk.Scale(window, 
+                      command = lambda value: print(scale_float.get()), 
+                      from_= -80, 
+                      to= 0,
+                    variable= scale_float )
+    scale.pack()
+
+    window.mainloop()
             
 
