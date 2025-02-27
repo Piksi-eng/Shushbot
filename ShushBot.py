8de5336bcc5f2e50d67b0ce160e17868b121edd7
Piksi-eng <bogosavljevicivan72@gmail.com>
2025-02-28 00:03:13 +0100

    Settings and configurations added

--- ShushBot.py
+++ ShushBot.py
@@ -10,7 +10,8 @@ import threading
 import pygame
 
 #Global Variables
-cooldown_time = 3  # Cooldown in seconds
+cooldown_time_sound = 3
+cooldown_time_flash = 0  # Cooldown in seconds
 last_played = 0  # Stores the last time a sound was played
 
 pygame.mixer.init()
@@ -65,6 +66,7 @@ def create_ring():
     return ring
 
     #depricated
+
 def play_sound(file_path):
     global last_played
     last_played = time.time()
@@ -91,7 +93,7 @@ def listen():
             progress_float.set(max_loudness/100)
 
             if(max_loudness > scale_float.get()):
-                if(playSound and (time.time() - last_played >= cooldown_time)):
+                if(playSound and (time.time() - last_played >= cooldown_time_sound)):
                     threading.Thread(target=play_soundv2, args=(sound_file,), daemon=True).start()
 
                 print(f"Maximum Loudness: {max_loudness:.2f} dB")
@@ -123,7 +125,19 @@ def toggleFlash():
 
 def validate_int_input(value):
     return value.isdigit() or value == ""
-# Example usage
+
+def update_cooldown_sound():
+    global cooldown_time_sound  
+    value = soundCoolDown.get()  
+    cooldown_time_sound = int(value) if value.isdigit() else 0  # Convert to int or default to 0
+    print(f"Updated cooldown: {cooldown_time_sound}")
+
+def update_cooldown_flash():
+    global cooldown_time_flash  
+    value = flashCoolDown.get()  
+    cooldown_time_flash = int(value) if value.isdigit() else 0  # Convert to int or default to 0
+    print(f"Updated cooldown: {cooldown_time_flash}")
+
 if __name__ == "__main__":
     #variables
     sound_file = 'Sounds\Pst.mp3'
@@ -132,7 +146,7 @@ if __name__ == "__main__":
 
     #window config
     window = CTk()
-    window.geometry("220x160")
+    window.geometry("300x250")
     window.title("ShushBot")
     #icon = tk.PhotoImage(file='Images\icon.png')
     #window.iconphoto(True, icon)
@@ -163,6 +177,8 @@ if __name__ == "__main__":
     flashLabel.pack()
     soundCoolDown = CTkEntry(soundFrame, placeholder_text="3",validate="key", validatecommand=(validate_int, "%P"))
     soundCoolDown.pack(side="right", padx=5, pady=5)
+    soundUpdate_button = CTkButton(soundFrame, text="Set", command=update_cooldown_sound)
+    soundUpdate_button.pack(side="right",pady=5)
 
     flashFrame = CTkFrame(notificationFrame)
     flashFrame.pack()
@@ -174,6 +190,8 @@ if __name__ == "__main__":
     flashLabel.pack()
     flashCoolDown = CTkEntry(flashFrame, placeholder_text="0", validate="key", validatecommand=(validate_int, "%P"))
     flashCoolDown.pack(side="right", padx=5, pady=5)
+    flashUpdate_button = CTkButton(flashFrame, text="Set", command=update_cooldown_flash)
+    flashUpdate_button.pack(side="right",pady=5)
 
 
 
@@ -183,7 +201,7 @@ if __name__ == "__main__":
     scaleFrame = CTkFrame(window)
     scaleFrame.pack()
     scaleInput = CTkEntry(scaleFrame, placeholder_text="80")
-    #scaleInput.pack()
+    scaleInput.pack()
 
     scale_float = tk.DoubleVar(value = 80)
     scale = CTkSlider(scaleFrame, 
